d076971b611b3cea555be00bfafe70324d12f8c7
Pingu9191 <99191235+Pingu9191@users.noreply.github.com>
2022-02-07 14:52:56 +0100

    Add files via upload

---
+++ game_reader.h
@@ -0,0 +1,16 @@
+/** 
+ * @brief It defines all necessary 
+ * to load the game
+ * @file game_reader.c
+ * @author Ignacio Nunnez && Nicolas Victorino
+ * @version 1.0 
+ * @date 03-02-2022 
+ * @copyright GNU Public License
+ */
+
+#include "command.h"
+#include "space.h"
+#include "types.h"
+#include "game.h"
+
+STATUS game_load_spaces(Game *game, char *filename);
