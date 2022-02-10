455dc2cb878fc511c14bf464db8c316c0899a9d3
Pingu9191 <99191235+Pingu9191@users.noreply.github.com>
2022-02-10 11:15:39 +0100

    Update game.h

--- game.h
+++ game.h
@@ -14,7 +14,6 @@
 #include "command.h"
 #include "space.h"
 #include "types.h"
-#include "object.h"
 
 typedef struct _Game
 {
@@ -24,14 +23,98 @@ typedef struct _Game
   T_Command last_cmd;
 } Game;
 
+/**
+  * @brief Creates the base of a new game
+  * @author Profesores PPROG
+  *
+  * Sets to NO_ID all ids for he game
+  * @param game a pointer to the game
+  * @return allways Ok
+  */
 STATUS game_create(Game *game);
+
+/**
+  * @brief Creates the game from a file
+  * @author Profesores PPROG
+  *
+  * Sets the game with the info of the filename
+  * @param game a pointer to the game
+  * @param filename exact name of the file 
+  * @return ERROR if no game or no filename
+  * 
+  */
 STATUS game_create_from_file(Game *game, char *filename);
+
+/**
+  * @brief Updates the game
+  * @author Profesores PPROG
+  *
+  * 
+  * @param game a pointer to the game
+  * @param cmd the cmd 
+  * @return allways Ok
+  */
 STATUS game_update(Game *game, T_Command cmd);
+
+/**
+  * @brief Removes all the info of the game
+  * @author Profesores PPROG
+  *
+  * Sets all spaces to NULL
+  * @param game a pointer to the game
+  * @return allways Ok
+  */
 STATUS game_destroy(Game *game);
+
+/**
+  * @brief Finish the game
+  * @author Profesores PPROG
+  *
+  * @param game a pointer to the game
+  * @return allways FALSE
+  */
 BOOL game_is_over(Game *game);
+
+/**
+  * @brief Prints useful info about
+  * the game
+  * @author Profesores PPROG
+  *
+  * @param game a pointer to the game
+  */
 void game_print_data(Game *game);
+
+/**
+  * @brief returns the space by its id
+  * @author Profesores PPROG
+  *
+  * @param game a pointer to the game
+  * @param id the id of the space
+  * @returns ERROR if no id
+  */
 Space *game_get_space(Game *game, Id id);
+
+/**
+  * @brief returns player-location id
+  * @author Profesores PPROG
+  *
+  * @param game a pointer to the game
+  */
 Id game_get_player_location(Game *game);
+
+/**
+  * @brief returns object-location id
+  * @author Profesores PPROG
+  *
+  * @param game a pointer to the game
+  */
 Id game_get_object_location(Game *game);
+
+/**
+  * @brief returns last cd_command
+  * @author Profesores PPROG
+  *
+  * @param game a pointer to the game
+  */
 T_Command game_get_last_command(Game *game);
 #endif
